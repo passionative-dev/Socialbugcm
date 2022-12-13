ab7f1c6869e8211ffb23381921f05c250af0918a
ruel121 <ruelavila121@gmail.com>
2022-12-13 15:36:06 +0200

    a

--- vendor/squizlabs/php_codesniffer/src/Standards/Generic/Tests/VersionControl/GitMergeConflictUnitTest.js
+++ vendor/squizlabs/php_codesniffer/src/Standards/Generic/Tests/VersionControl/GitMergeConflictUnitTest.js
@@ -1,19 +1,10 @@
-
 result = x?y:z;
 result = x ? y : z;
 
-<<<<<<< HEAD
-if (something === true
-=======
 if (something === false
->>>>>>> develop
     ^ somethingElse === true
 ) {
-<<<<<<< HEAD
-    return true;
-=======
     return false;
->>>>>>> develop
 }
 
 y = 1
@@ -21,13 +12,5 @@ y = 1
   - 3;
 
 /*
-<<<<<<< HEAD
- * @var string $bar
- */
-if (something === true
-
-/**
-=======
  * @var string $foo
->>>>>>> master
  */
